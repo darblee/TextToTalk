d47565369a32d4cf5174326a78fa7ba9f06a570f
david-lee <darblee123@gmail.com>
2023-11-23 18:20:00 -0800

    Initial commit

---
+++ app/src/main/java/com/darblee/texttotalk/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.darblee.texttotalk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.widget.Toast
+import com.darblee.texttotalk.databinding.ActivityMainBinding
+import java.util.Locale
+
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    private var tts: TextToSpeech? = null
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        tts = TextToSpeech(this, this)
+
+        binding.btnSpeak.setOnClickListener {
+            speakOut("Hello World")
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts!!.setLanguage(Locale.US)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "The language specified is not supported")
+            }
+        } else {
+            Log.e("TTS", "Text To Speech Initialization failed")
+        }
+    }
+
+    private fun speakOut(text: String) {
+        tts?.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (tts != null) {
+            tts?.stop()
+            tts?.shutdown()
+        }
+    }
+
+}
